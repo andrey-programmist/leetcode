489ccd24eafcac5430f178a637163b9beb0fa8f7
a.ashurko <andrey_programmist@hotmail.com>
2018-03-27 17:37:34 +0300

    reverse integer

---
+++ reverse-integer.js
@@ -0,0 +1,18 @@
+const inputs = [123, -123, 120, 0];
+
+const max32 = Math.pow(2, 31) - 1;
+const min32 = ~max32;
+const reverse = (x) => {
+    const splitted = x.toString().split('');
+    const result = [];
+    for (let i = splitted.length - 1; i > 0; i--) {
+        result.push(splitted[i]);
+    }
+    if (splitted[0] === '-') result.unshift(splitted[0]);
+    else result.push(splitted[0]);
+    const intRes = parseInt(result.join(''));
+    if (intRes >= min32 && intRes <= max32) return intRes;
+    return 0;
+};
+
+console.log(inputs.map(i => reverse(i)).join());
