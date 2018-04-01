11b548f8ce4069e201edd43f1fc7a54e003c85ad
Andrey Ashurko <andrey_programmist@hotmail.com>
2018-04-01 21:11:57 +0300

    merge two sorted lists: iterative, recursive

---
+++ merge-two-sorted-lists.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+const mergeTwoLists = function (l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let head = null;
+  if (l1.val < l2.val) {
+    head = l1;
+    l1 = l1.next;
+  } else {
+    head = l2;
+    l2 = l2.next;
+  }
+
+  let curNode = head;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curNode.next = l1;
+      l1 = l1.next;
+    } else {
+      curNode.next = l2;
+      l2 = l2.next;
+    }
+    curNode = curNode.next;
+  }
+
+  if (l1) curNode.next = l1;
+  else curNode.next = l2;
+
+  return head;
+};
--- palindrome-number.js
+++ palindrome-number.js
@@ -2,22 +2,44 @@ const numbers = [0, 1, 2, 3, 4, 10, 11, 12, 111, 242, 233, 10021, 10501, 4224];
 
 const max32 = 0x7fffffff;
 const isPalindrome = x => {
-    if (!(x >= 0 && x <= max32)) return false;
-    let step = 0;
-    while (true) {
-        if (x / Math.pow(10, step) >= 10) {
-            step += 1;
-        }
-        else break;
+  if (!(x >= 0 && x <= max32)) return false;
+  let revNumber = 0;
+  let t = 0;
+  while (true) {
+    revNumber *= 10;
+    const pow = Math.pow(10, ~~Math.log10(revNumber));
+    revNumber += (~~(x / pow) % 10);
+    console.log(`pow ${pow}; rev ${revNumber}`);
+    if (x < pow * 10) break;
+    for (i = 0; i < 100000000; i++) {
+      t += Math.sqrt(i + Math.random());
     }
-    if (step === 0) return true;
-    step += 1;
-    for (let i = 1; i < ~~(step / 2) + 1; i++) {
-        const left = ((~~(x / Math.pow(10, step - i))) % 10);
-        const right = ~~(x % Math.pow(10, i) / Math.pow(10, i - 1));
-        if (left !== right) return false;
-    }
-    return true;
+  }
+  return x === revNumber;
 };
 
-numbers.forEach(i => console.log(`${i};${isPalindrome(i)}`));
+const isPalindromeOld = x => {
+  if (!(x >= 0 && x <= max32)) return false;
+  let step = 0;
+  while (true) {
+    if (x / Math.pow(10, step) >= 10) {
+      step += 1;
+    }
+    else break;
+  }
+  if (step === 0) return true;
+  step += 1;
+  for (let i = 1; i < ~~(step / 2) + 1; i++) {
+    const left = ((~~(x / Math.pow(10, step - i))) % 10);
+    const right = ~~(x % Math.pow(10, i) / Math.pow(10, i - 1));
+    if (left !== right) return false;
+  }
+  return true;
+};
+
+// 1234567
+
+
+console.log(isPalindrome(100));
+
+// numbers.forEach(i => console.log(`${i};${isPalindrome(i)}`));
